5cf2dcefcc5e8df1877b9bc8d4c31ce06bbb5b5c
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-22 14:40:17 +0300

    Updated file

--- W1_A1_Q3_Spencer.py
+++ W1_A1_Q3_Spencer.py
@@ -1,23 +1,31 @@
 
-''' The program asks you 10 questions about ALU.
-If you miss six questions you have failed the game. 
+"""Question 3: ALU Hangman
+The program asks you 10 questions about ALU.
+
+Every time you miss a question (you are hanging the man).
+
+If you miss six questions (the man dies). 
+
 And the game stops.
-If you get all the questions right, you get congratulated and the game stops
-;If you finish all the questions with less than 6 mistakes, you are shown your score, and you are told the number of mistakes you made, and the game stops.
-When was ALU founded (2015)
-Who is the CEO of ALU (Fred Swaniker)
-Where are ALU campuses (Rwanda and Mauritius)
-How many campuses does ALU have (2)
-What is the name of ALU Rwandaâ€™s Dean (Gaidi Faraj)
-Who is in Charge of Student Life (Sila Ogidi)
-What is the name of our Lab (Nigeria)
-How many students do we have in year 2 Cs (put the number of students)
-How many degrees does ALU offer (8)
-Where are the headquarters of ALU (Mauritius)
-Handle user input well eg: Fred Swaniker , FRed Swaniker , fred swaniker and FRED SWANIKER are the right answer , meaning you ignore the case (upper or lower) while dealing with the strings here )
-'''
+
+If you get all the questions right, your man lives, you get congratulated, and the game stops. 
+
+If you finish all the questions with less than 6 mistakes, your man lives, and you are told the number of mistakes you made, and the game stops.
+
+Questions to be asked:
+
+When was ALU founded (2015)?
+Who is the CEO of ALU (Fred Swaniker)?
+Where are ALU campuses (Rwanda and Mauritius)?
+How many campuses does ALU have (2)?
+What is the name of ALU Rwanda’s Dean (Insert dean’s name)?
+Who is in charge of Student Life (Sila Ogidi)?
+What is the name of our Lab (Insert name here)?
+How many students do we have in Year 2 CS(put the number of students)?
+How many degrees does ALU offer (8)?
+Where are the headquarters of ALU (Mauritius)?"""
 import time
-input("Welcome to ALU Trivia. Press enter to begin game..")
+input("Welcome to ALU Hungman. Press enter to begin game..")
 wrong =0
 right =0
 
@@ -80,7 +88,7 @@ try:
             fail()   
 
         q8 = int(input("How many students do we have in year 2 Cs : "))
-        if q8 == 200 :
+        if q8 == 96 :
             right += 1
             print(f"right8 = {right}")
         else:
@@ -106,7 +114,7 @@ try:
             finish()
     def fail():
         if wrong == 6 :
-            print("Sorry, you have failed the game.")
+            print("Sorry, you have failed the game.\nThe man ha died")
             exit()
         
         else:
